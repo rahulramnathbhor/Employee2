ef51928680aa532eaa395b02ae0b7847c97c9bcd
rahulramnathbhor <112869809+rahulramnathbhor@users.noreply.github.com>
2022-09-16 15:54:29 +0530

    Add project files.

---
+++ Employee2/Employee.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee2
+{
+    internal class Employee
+    {
+        protected int id;
+        private static int count = 0;
+        protected string name;
+        protected double bs, hra, da, pt, pf, total, netpaid;
+        public Employee( string name, double bs,  double pt)
+
+        {
+            count++;
+            id=count;
+           
+            this.name = name;
+            this.bs = bs;
+           
+            this.pt = pt;
+          
+        }
+        public static int DisplayCount()
+        {
+
+            return count;
+        }
+        public virtual void CalculateSalary()
+        {
+            hra = bs * 0.40;
+            da = bs * 0.20;
+            pf = bs * 0.12;
+            total = bs + hra + da;
+            netpaid = total - (pf + pt);
+        }
+        public override string ToString()
+        {
+            return $"Employee Id {id} Employee Name{name} Total salary{total} Netpaid {netpaid}";
+        }
+        public class Manager : Employee
+        {
+            private double ta;
+          
+              
+                public Manager(string name, double bs, double pt, double ta) : base(name, bs, pt) 
+                {
+                    this.ta = ta;
+                }
+                public override void CalculateSalary()
+                {
+                    hra = bs * 0.40;
+                    da = bs * 0.20;
+                    pf = bs * 0.12;
+                    total = bs + hra + da + ta;
+                    netpaid = total - (pf + pt);
+                }
+                public override string ToString()
+                {
+                    return $"Manager Id {id} Manager name {name} , total salary {total} & Net paid {netpaid}";
+                }
+            
+
+        }
+
+    }
+}
---
+++ Employee2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee("abc", 34000, 200);
+            e1.CalculateSalary();
+            Console.WriteLine(e1);
+        }
+    }
+}
